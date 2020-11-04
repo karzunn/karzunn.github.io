189661014d00fe42e66b8f969608e1aaa13f43b8
karzunn <offillcarson@gmail.com>
2020-11-04 16:05:27 -0500

    un-commented script

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-/*
 Blackbaud_Init({
     key: "2d91906c-4f0a-4cc2-b6c9-b383056e577b",
     merchant_account_id: "258dc416-b21d-4c10-b04f-7686e08a9ca1",
     transaction_type: "store_card_info"
 });
-
-
-Blackbaud_Open({
-    amount: '1.00',
-}
-);
 
 
 Blackbaud_OpenStoreCardForm(
@@ -23,4 +16,3 @@ document.addEventListener('checkoutComplete', function (event) {
     console.log('Token: ', event.detail.transactionToken);
 });
 
-*/
