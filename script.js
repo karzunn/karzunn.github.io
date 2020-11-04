0e9447638aa94af5f8d5d469b3413e80d6aed022
karzunn <offillcarson@gmail.com>
2020-11-04 14:48:04 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+Blackbaud_Init({
+    key: "2d91906c-4f0a-4cc2-b6c9-b383056e577b",
+    merchant_account_id: "258dc416-b21d-4c10-b04f-7686e08a9ca1",
+    transaction_type: "card_not_present"
+});
+
+Blackbaud_Open(
+    {
+    amount: '12.00'
+    }  
+);
