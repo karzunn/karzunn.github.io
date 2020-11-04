ea27c8da186be70a8666f45e2356240e9c2099b0
karzunn <offillcarson@gmail.com>
2020-11-04 16:20:54 -0500

    changed transaciton type

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 Blackbaud_Init({
     key: "2d91906c-4f0a-4cc2-b6c9-b383056e577b",
     merchant_account_id: "258dc416-b21d-4c10-b04f-7686e08a9ca1",
-    transaction_type: "store_card_info"
+    transaction_type: "store_card"
 });
 
 
