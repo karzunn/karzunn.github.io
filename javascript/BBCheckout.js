b2b7cd35b13a853b2ec17481232729afbd41dc18
karzunn <offillcarson@gmail.com>
2020-11-05 11:40:05 -0500

    attempt at fixing checkout window inconsistency

--- javascript/BBCheckout.js
+++ javascript/BBCheckout.js
@@ -1,11 +1,4 @@
-Blackbaud_Init({
-    key: "2d91906c-4f0a-4cc2-b6c9-b383056e577b",
-    merchant_account_id: "258dc416-b21d-4c10-b04f-7686e08a9ca1",
-    transaction_type: "store_card"
-});
-
-
-Blackbaud_OpenStoreCardForm(
+document.onload = Blackbaud_OpenStoreCardForm(
     {
         card_token: "ce53bcff-c26d-4c8e-af6c-b54d038ef776",
         key: "2d91906c-4f0a-4cc2-b6c9-b383056e577b",
@@ -15,6 +8,12 @@ Blackbaud_OpenStoreCardForm(
         font_family: "sans-serif"
     }
 )
+
+Blackbaud_Init({
+    key: "2d91906c-4f0a-4cc2-b6c9-b383056e577b",
+    merchant_account_id: "258dc416-b21d-4c10-b04f-7686e08a9ca1",
+    transaction_type: "store_card"
+});
 
 document.addEventListener('checkoutComplete', function (event) {
     console.log('Token: ', event.detail.transactionToken);
